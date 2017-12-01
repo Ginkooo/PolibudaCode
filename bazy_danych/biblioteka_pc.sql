5f32490e773909af960ff016e125ce2b6731e91f
student <student@cs.pollub.pl>
2017-12-01 11:46:13 +0100

    dfdf

--- bazy_danych/biblioteka_pc.sql
+++ bazy_danych/biblioteka_pc.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2017 at 12:00 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Czas generowania: 01 Gru 2017, 11:45
+-- Wersja serwera: 10.1.16-MariaDB
+-- Wersja PHP: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `biblioteka_pc`
+-- Baza danych: `biblioteka_pc`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `books`
+-- Struktura tabeli dla tabeli `books`
 --
 
 CREATE TABLE `books` (
@@ -35,7 +35,7 @@ CREATE TABLE `books` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `books`
+-- Zrzut danych tabeli `books`
 --
 
 INSERT INTO `books` (`id`, `title`, `release_date`, `genre`, `author`) VALUES
@@ -49,7 +49,7 @@ INSERT INTO `books` (`id`, `title`, `release_date`, `genre`, `author`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `borrows`
+-- Struktura tabeli dla tabeli `borrows`
 --
 
 CREATE TABLE `borrows` (
@@ -62,7 +62,7 @@ CREATE TABLE `borrows` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `borrows`
+-- Zrzut danych tabeli `borrows`
 --
 
 INSERT INTO `borrows` (`id`, `book`, `student`, `employee`, `borrowing_date`, `date_of_return`) VALUES
@@ -70,12 +70,13 @@ INSERT INTO `borrows` (`id`, `book`, `student`, `employee`, `borrowing_date`, `d
 (2, 1, 4, 2, '2017-11-23', '2018-01-24'),
 (3, 6, 1, 4, '2017-11-24', '2018-01-24'),
 (4, 5, 4, 3, '2017-11-24', '2018-01-24'),
-(5, 3, 3, 3, '2017-11-06', '2017-11-15');
+(5, 3, 3, 3, '2017-11-06', '2017-11-15'),
+(14, 4, 1, 2, '2017-12-01', '2018-01-10');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employees`
+-- Struktura tabeli dla tabeli `employees`
 --
 
 CREATE TABLE `employees` (
@@ -87,23 +88,24 @@ CREATE TABLE `employees` (
   `employment_date` date NOT NULL,
   `birth_date` date NOT NULL,
   `city` varchar(25) COLLATE utf8_polish_ci NOT NULL,
-  `street` varchar(70) COLLATE utf8_polish_ci NOT NULL
+  `street` varchar(70) COLLATE utf8_polish_ci NOT NULL,
+  `postal_code` varchar(30) COLLATE utf8_polish_ci DEFAULT '34-523'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `employees`
+-- Zrzut danych tabeli `employees`
 --
 
-INSERT INTO `employees` (`id`, `firstname`, `lastname`, `phone_nr`, `pesel`, `employment_date`, `birth_date`, `city`, `street`) VALUES
-(1, 'Piotr', 'Czajka', 2147483647, 52547421464, '2010-05-11', '1991-09-12', 'Lublin', 'Króla Rogera'),
-(2, 'Michał', 'Wawrzyk', 2147483647, 52637485944, '2011-05-11', '1990-09-12', 'Lublin', 'Polna'),
-(3, 'Kuba', 'Marcyniuk', 2147483647, 74784632436, '2015-05-11', '1989-09-12', 'Lublin', 'Malinowa'),
-(4, 'Michał', 'Wawrzyk', 2147483647, 62859372849, '2014-05-11', '1988-09-12', 'Lublin', 'Jana pawła II');
+INSERT INTO `employees` (`id`, `firstname`, `lastname`, `phone_nr`, `pesel`, `employment_date`, `birth_date`, `city`, `street`, `postal_code`) VALUES
+(1, 'Piotr', 'Czajka', 2147483647, 52547421464, '2010-05-11', '1991-09-12', 'Lublin', 'Króla Rogera', '34-523'),
+(2, 'Michał', 'Wawrzyk', 2147483647, 52637485944, '2011-05-11', '1990-09-12', 'Lublin', 'Polna', '34-523'),
+(3, 'Kuba', 'Marcyniuk', 2147483647, 74784632436, '2015-05-11', '1989-09-12', 'Lublin', 'Malinowa', '34-523'),
+(4, 'Michał', 'Wawrzyk', 2147483647, 62859372849, '2014-05-11', '1988-09-12', 'Lublin', 'Jana pawła II', '34-523');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `students`
+-- Struktura tabeli dla tabeli `students`
 --
 
 CREATE TABLE `students` (
@@ -120,7 +122,7 @@ CREATE TABLE `students` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
--- Dumping data for table `students`
+-- Zrzut danych tabeli `students`
 --
 
 INSERT INTO `students` (`id`, `index_nr`, `firstname`, `lastname`, `birth_date`, `pesel`, `city`, `street`, `apartment`, `phone_nr`) VALUES
@@ -129,10 +131,11 @@ INSERT INTO `students` (`id`, `index_nr`, `firstname`, `lastname`, `birth_date`,
 (3, 8234, 'Michał', 'Błaszczyk', '1996-04-04', 2147483647, 'Lublin', 'Organowa', '3', 2147483647),
 (4, 8234, 'Kuba', 'Marcyniuk', '1993-04-05', 2147483647, 'Lublin', 'Organowa', '3', 2147483647),
 (5, 8234, 'Filip', 'Demski', '1996-04-11', 2147483647, 'Lublin', 'Organowa', '3', 2147483647),
-(6, 8234, 'Kuba', 'Marcyniuk', '1997-07-05', 2147483647, 'Lublin', 'Magnoliowa', '64', 2147483647);
+(6, 8234, 'Kuba', 'Marcyniuk', '1997-07-05', 2147483647, 'Lublin', 'Magnoliowa', '64', 2147483647),
+(7, 83730, 'Czajka', 'Piotr', '0000-00-00', 2147483647, 'Lublin', 'Króla Rogera 3', '64', 786818448);
 
 --
--- Indexes for dumped tables
+-- Indeksy dla zrzutów tabel
 --
 
 --
@@ -165,25 +168,25 @@ ALTER TABLE `students`
 --
 
 --
--- AUTO_INCREMENT for table `books`
+-- AUTO_INCREMENT dla tabeli `books`
 --
 ALTER TABLE `books`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
--- AUTO_INCREMENT for table `borrows`
+-- AUTO_INCREMENT dla tabeli `borrows`
 --
 ALTER TABLE `borrows`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
--- AUTO_INCREMENT for table `employees`
+-- AUTO_INCREMENT dla tabeli `employees`
 --
 ALTER TABLE `employees`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `students`
+-- AUTO_INCREMENT dla tabeli `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
