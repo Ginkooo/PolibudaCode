e1fb12db26925ef2a77df49b19fc7beae813f97e
Piotr Czajka <czajka@protonmail.com>
2018-01-16 20:15:00 +0100

    DB created and configured

--- TAI/projekt/config/app.php
+++ TAI/projekt/config/app.php
@@ -221,17 +221,13 @@ return [
         'default' => [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
-            'persistent' => false,
-            'host' => 'localhost',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'database' => 'shop.db',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
