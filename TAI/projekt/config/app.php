45779cf64c66ade4d004537388b5e7e49a6035a9
Piotr Czajka <czajka@protonmail.com>
2018-01-16 20:16:14 +0100

    DB driver set to sqlite

--- TAI/projekt/config/app.php
+++ TAI/projekt/config/app.php
@@ -220,7 +220,7 @@ return [
     'Datasources' => [
         'default' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
+            'driver' => 'Cake\Database\Driver\Sqlite',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
